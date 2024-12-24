acbe3858dd648b494141179d8c1100f309fd6478
mehrdad <mehrdadsadeghi769@gmail.com>
2024-12-24 05:24:13 -0800

    Update mongose.js

--- src/utills/common/mongose.js
+++ src/utills/common/mongose.js
@@ -1,9 +1,9 @@
 
 const mongoose = require('mongoose');
-
+const MONGODB_URI='mongodb://root:1ghLoIcLjglaKTKEk6Kk554I@said:27017/my-app?authSource=admin'
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
