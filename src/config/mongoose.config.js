e8d841f73ce701fd0ff696af7aa595a5c3c23302
mehrdad <mehrdadsadeghi769@gmail.com>
2024-12-26 08:37:42 -0800

    +

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -1,13 +1,11 @@
 const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 const tempText="Mongoose status is : "
-MONGODB_URI_LOCAL="mongodb://localhost:27017"
 
-//const monogourl="mongodb://root:1ghLoIcLjglaKTKEk6Kk554I@said:27017/my-app?authSource=admin";
-const monogourl ="mongodb://root:1ghLoIcLjglaKTKEk6Kk554I@makalu.liara.cloud:31185/my-app?authSource=admin";
+const url =process.env.URL
 
 try {
-    mongoose.connect(MONGODB_URI_LOCAL);
+    mongoose.connect(url);
     mongoose.connection.on('connected', () => console.log(tempText,'connected'));
     mongoose.connection.on('open', () => console.log(tempText,'open'));
     mongoose.connection.on('disconnected', () => console.log(tempText,'disconnected'));
